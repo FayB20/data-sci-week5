f0605a392fceb7f8a909d1424d577cebe12735b2
Fay Bennett <feb518@york.ac.uk>
2020-10-30 10:59:13 +0000

    removing NA

--- data-sci-week5.R
+++ data-sci-week5.R
@@ -1,3 +1,13 @@
 # Reading the file in:
-file <- "Human-development-index.csv"
-hdi <- read_table2(file)
+hdi <- read.csv("Human-development-index.csv")
+
+# tidying the hdi data:
+hdi2 <- hdi %>% 
+  pivot_longer(names_to = "Years",
+               values_to = "Index",
+               cols = -c(Country,HDI.Rank..2018.))
+
+# filtering the dataset to exclude the missing observations:
+hdi3 <- hdi2[!is.na(hdi2$Index),]
+hdi4 <- hdi3[!is.na(hdi3$HDI.Rank..2018.),]
+
