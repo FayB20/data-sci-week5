a38bba5f618eaefc9bce09bffeec4aec10cd9103
Fay Bennett <feb518@york.ac.uk>
2020-10-30 11:20:41 +0000

    Creating a plot for the data

--- data-sci-week5.R
+++ data-sci-week5.R
@@ -23,5 +23,23 @@ hdi3_summary <- hdi3 %>%
             sd = sd(Index),
             se = sd/sqrt(n))
 
+# filtering the summary to get just the ten countries with the lowest mean HDI:
+hdi4_summary <- hdi3_summary %>% 
+  filter(rank(mean_index) < 11)
+hdi4_summary
 
-
+# plotting the data:
+hdi4_summary %>% 
+  ggplot() +
+  geom_point(aes(x = Country,
+                 y = mean_index)) +
+  geom_errorbar(aes(x = Country,
+                    ymin = mean_index - se,
+                    ymax = mean_index + se)) +
+  scale_y_continuous(limits = c(0, 0.5),
+                     expand = c(0, 0),
+                     name = "HDI") +
+  scale_x_discrete(expand = c(0, 0),
+                   name = "") +
+  theme_classic() +
+  coord_flip()
